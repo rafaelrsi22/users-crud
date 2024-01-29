d64662d98aa424ef17bb322bdaea4f3af17e4ee8
Rafael Godoy <rafaelrsi108@gmail.com>
2024-01-29 20:58:21 -0300

    Username UPDATE skeleton done

--- public/scripts/usersManager.js
+++ public/scripts/usersManager.js
@@ -2,22 +2,40 @@ const usersTable = document.getElementById('users-table');
 
 function insertUserRow(info, onDelete) {
     const tableRow = document.createElement('tr');
+    const usernameInput = document.createElement('input');
     const usernameCell = document.createElement('td');
     const creationCell = document.createElement('td');
     const updateCell = document.createElement('td');
     const deleteCell = document.createElement('td');
     const deleteButton = document.createElement('button');
 
+    let lastUsernameInput = info.username;
+
     tableRow.id = `user-${info.id}`;
 
-    usernameCell.innerText = info.username;
+    usernameInput.value = info.username;
     creationCell.innerText = info.created_at;
     updateCell.innerText = info.updated_at;
     deleteButton.innerText = "Delete";
 
+    usernameInput.autocomplete = "off";
+    usernameInput.classList.add('input-read');
+    usernameInput.type = "text";
+
     deleteButton.addEventListener('click', onDelete);
 
+    usernameInput.addEventListener('focusout', () => {
+        usernameInput.value = lastUsernameInput;
+    });
+
+    usernameInput.addEventListener('keypress', (e) => {
+        if (e.key === 'Enter') {
+            console.log('submit')
+        }
+    });
+
     deleteCell.appendChild(deleteButton);
+    usernameCell.appendChild(usernameInput);
     tableRow.append(usernameCell, creationCell, updateCell, deleteCell);
 
     usersTable.appendChild(tableRow);
