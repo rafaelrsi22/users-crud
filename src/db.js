ed6cc44de92eb5ef6721538adc6c8540d31bf3bc
Rafael Godoy <rafaelrsi108@gmail.com>
2024-01-29 14:53:11 -0300

    Fixed db connection bugs

--- src/db.js
+++ src/db.js
@@ -1,9 +1,16 @@
 const mysql = require('mysql2');
 
-const pool = mysql.createPool({
+const connection = mysql.createConnection({
     port: process.env.DB_PORT,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 }).promise();
+
+async function testing() {
+    const query = await connection.query("SELECT * FROM users");
+    console.log(query);
+}
+
+testing();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
+
 const express = require('express');
 const db = require('./db');
-const dotenv = require('dotenv').config();
 
 const app = express();
 
